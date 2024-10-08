0a1eb271b017f8139882ab36e8960f6f33e26098
Seongmo Ahn <djgood780@gmail.com>
2024-10-08 16:13:01 +0900

    ãfeat: 전체 충전기 조회 ê¸ 기능 추가 resolves: #2

--- src/main/java/com/example/happyDream/Service/ChargerService.java
+++ src/main/java/com/example/happyDream/Service/ChargerService.java
@@ -1,8 +1,12 @@
 package com.example.happyDream.Service;
 
+import com.example.happyDream.Entity.ChargerEntity;
 import com.example.happyDream.Repository.ChargerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.example.happyDream.DTO.ChargerDto;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ChargerService {
@@ -11,5 +15,24 @@ public class ChargerService {
     @Autowired
     public ChargerService(ChargerRepository chargerRepository) {
         this.chargerRepository = chargerRepository;
+    }
+
+    public List<ChargerDto> convertEntityListToDtoList(List<ChargerEntity> entityList) {
+        // Stream을 사용하여 Entity -> Dto 변환 후 리스트로 반환
+        return entityList.stream()
+                .map(ChargerEntity::toDto) // 각 엔티티를 DTO로 변환
+                .collect(Collectors.toList());  // 변환된 결과를 리스트로 수집
+    }
+
+    public List<ChargerEntity> convertDtoListToEntityList(List<ChargerDto> dtoList) {
+        // Stream을 사용하여 Entity -> Dto 변환 후 리스트로 반환
+        return dtoList.stream()
+                .map(ChargerDto::toEntity) // 각 엔티티를 DTO로 변환
+                .collect(Collectors.toList());  // 변환된 결과를 리스트로 수집
+    }
+
+    public List<ChargerDto> chargerSelectAll(){
+        List<ChargerDto> dtoList = convertEntityListToDtoList(this.chargerRepository.findAll());
+        return dtoList;
     }
 }
