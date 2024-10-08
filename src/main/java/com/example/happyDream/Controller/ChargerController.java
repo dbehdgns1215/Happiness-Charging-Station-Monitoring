2a7958c123bc97c5c944f77816b7820da5658e61
Seongmo Ahn <djgood780@gmail.com>
2024-10-08 17:07:05 +0900

    feat: 전체 충전기 삭제 기외 2개 기능 추가 1) 전체 충전기 삭제 기능 추가 ì완료 2) 특정 충전기 조회 기능 추가 완료 3) 특정 충전기 삭제 기능 추가 완료 resolves: #3

--- src/main/java/com/example/happyDream/Controller/ChargerController.java
+++ src/main/java/com/example/happyDream/Controller/ChargerController.java
@@ -5,7 +5,10 @@ import com.example.happyDream.Entity.ChargerEntity;
 import com.example.happyDream.Service.ChargerServiceFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -18,9 +21,40 @@ public class ChargerController {
         this.chargerServiceFacade = chargerServiceFacade;
     }
 
+    //전체 충전기 조회
     @GetMapping("/chargers")
     public String chargerSelectAll() {
         List<ChargerDto> chargers = this.chargerServiceFacade.chargerSelectAll();
+        return " ";
+    }
+
+    //전체 충전기 삭제
+    @DeleteMapping("/chargers")
+    public String chargerDeleteAll() {
+        this.chargerServiceFacade.chargerDeleteAll();
+        return " ";
+    }
+
+    //충전기 추가
+    /*
+    @PostMapping("/chargers")
+    public String chargerInsert(@RequestParam(value="")) {
+
+        return " ";
+    }
+    */
+
+    //특정 충전기 조회
+    @GetMapping("/chargers/{id}")
+    public String chargerSelect(@PathVariable("id") Integer id) {
+        ChargerDto charger = this.chargerServiceFacade.chargerSelect(id);
+        return " ";
+    }
+
+    //특정 충전기 삭제
+    @GetMapping("/chargers/{id}")
+    public String chargerDelete(@PathVariable("id") Integer id) {
+        this.chargerServiceFacade.chargerDelete(id);
         return " ";
     }
 }
