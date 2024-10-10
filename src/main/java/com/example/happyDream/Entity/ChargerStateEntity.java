ef27f21f677f88989fd0ab26a35de84255281754
dbehdgns1215 <77192122+dbehdgns1215@users.noreply.github.com>
2024-10-10 22:49:32 +0900

    chore: 오타 수정

--- src/main/java/com/example/happyDream/Entity/ChargerStateEntity.java
+++ src/main/java/com/example/happyDream/Entity/ChargerStateEntity.java
@@ -1,7 +1,6 @@
 package com.example.happyDream.Entity;
 
 import com.example.happyDream.DTO.ChargerStateDTO;
-import com.example.happyDream.DTO.ChargerStateDto;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
