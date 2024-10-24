1646d2f41ff071081c800bc39c80814e656f1c9e
Seongmo Ahn <djgood780@gmail.com>
2024-10-24 13:29:20 +0900

    feat(UserController): 전체 사용자 조회 기능 추가

--- src/main/java/com/example/happyDream/Controller/UserController.java
+++ src/main/java/com/example/happyDream/Controller/UserController.java
@@ -1,8 +1,14 @@
 package com.example.happyDream.Controller;
 
+import com.example.happyDream.DTO.ChargerDTO;
+import com.example.happyDream.DTO.UserDTO;
 import com.example.happyDream.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -12,4 +18,12 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
+    //전체 사용자 조회
+    @GetMapping("/users")
+    public String userSelectAll(Model model) {
+        List<UserDTO> users = this.userService.userSelectAll();
+        return " ";
+    }
+
 }
