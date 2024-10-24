10f1100c29cd37e3de4f0172078c365736c3687b
Seongmo Ahn <djgood780@gmail.com>
2024-10-24 13:48:07 +0900

    feat(ReviewController): 전체 리뷰 조회 기능 추가

--- src/main/java/com/example/happyDream/Controller/ReviewController.java
+++ src/main/java/com/example/happyDream/Controller/ReviewController.java
@@ -1,8 +1,13 @@
 package com.example.happyDream.Controller;
 
+import com.example.happyDream.DTO.ReviewDTO;
 import com.example.happyDream.Service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class ReviewController {
@@ -11,5 +16,12 @@ public class ReviewController {
     @Autowired
     public ReviewController(ReviewService reviewService) {
         this.reviewService = reviewService;
+    }
+
+    //전체 리뷰 조회
+    @GetMapping("/users")
+    public String reviewSelectAll(Model model) {
+        List<ReviewDTO> users = this.reviewService.reviewSelectAll();
+        return " ";
     }
 }
