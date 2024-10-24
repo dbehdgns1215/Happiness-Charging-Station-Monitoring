9f75ad49124d480df49e9236b29ef33572f43d2e
Seongmo Ahn <djgood780@gmail.com>
2024-10-24 13:56:46 +0900

    feat(ReviewController): 특정 리뷰 수정 기능 추가

--- src/main/java/com/example/happyDream/Controller/ReviewController.java
+++ src/main/java/com/example/happyDream/Controller/ReviewController.java
@@ -1,6 +1,7 @@
 package com.example.happyDream.Controller;
 
 import com.example.happyDream.DTO.ReviewDTO;
+import com.example.happyDream.DTO.UserDTO;
 import com.example.happyDream.Service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,27 +20,38 @@ public class ReviewController {
     }
 
     //전체 리뷰 조회
-    @GetMapping("/users")
+    @GetMapping("/reviews")
     public String reviewSelectAll(Model model) {
-        List<ReviewDTO> users = this.reviewService.reviewSelectAll();
+        List<ReviewDTO> reviews = this.reviewService.reviewSelectAll();
         return " ";
     }
 
     //전체 리뷰 삭제
-    @DeleteMapping("/users")
+    @DeleteMapping("/reviews")
     public String reviewDeleteAll() {
         this.reviewService.reviewDeleteAll();
         return " ";
     }
 
     //리뷰 추가
-    @PostMapping("/users")
+    @PostMapping("/reviews")
     public String reviewInsert(@RequestParam(value="charger_id") Integer chargerId,
                              @RequestParam(value="user_id") Integer userId,
-                             @RequestParam(value="review_content") String email,
-                             @RequestParam(value="rating") Byte userType,
+                             @RequestParam(value="review_content") String content,
+                             @RequestParam(value="rating") Byte rating,
                              @RequestParam(value="deleted_yn") Boolean deletedYn) {
-        this.reviewService.reviewInsert(chargerId, userId, email, userType, deletedYn);
+        this.reviewService.reviewInsert(chargerId, userId, content, rating, deletedYn);
+        return " ";
+    }
+
+    //특정 리뷰 수정
+    @PostMapping("/reviews/{id}")
+    public String reviewUpdate(@RequestParam(value="charger_id") Integer chargerId,
+                               @RequestParam(value="user_id") Integer userId,
+                               @RequestParam(value="review_content") String content,
+                               @RequestParam(value="rating") Byte rating,
+                               @RequestParam(value="deleted_yn") Boolean deletedYn) {
+        this.reviewService.reviewUpdate(chargerId, userId, content, rating, deletedYn);
         return " ";
     }
 }
