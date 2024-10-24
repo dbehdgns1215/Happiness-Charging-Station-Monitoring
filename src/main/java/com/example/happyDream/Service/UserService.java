af28a91f84831e5671ee06a2233875a62cc75315
Seongmo Ahn <djgood780@gmail.com>
2024-10-24 13:37:40 +0900

    feat(UserService): 전체 사용자 조회 기능 추가

--- src/main/java/com/example/happyDream/Service/UserService.java
+++ src/main/java/com/example/happyDream/Service/UserService.java
@@ -1,8 +1,19 @@
 package com.example.happyDream.Service;
 
+import com.example.happyDream.DTO.ChargerDTO;
+import com.example.happyDream.DTO.UserDTO;
+import com.example.happyDream.Entity.ChargerEntity;
+import com.example.happyDream.Entity.UserEntity;
 import com.example.happyDream.Repository.UserRepository;
+import com.example.happyDream.Util.Converter;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -12,4 +23,10 @@ public class UserService {
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
+    public List<UserDTO> userSelectAll() {
+        List<UserEntity> entityList = this.userRepository.findAll();
+        return Converter.EntityListToDtoList(entityList, UserEntity::toDTO);
+    }
+
 }
