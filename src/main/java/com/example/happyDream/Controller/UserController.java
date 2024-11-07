e60f0acd8bae0c5b250c6908b872d654e7fb8217
Seongmo Ahn <djgood780@gmail.com>
2024-11-08 02:11:13 +0900

    refactor: 원격 저장소의 ChargerService와 로컬 저장소의 ChargerService를 병합

--- src/main/java/com/example/happyDream/Controller/UserController.java
+++ src/main/java/com/example/happyDream/Controller/UserController.java
@@ -2,6 +2,9 @@ package com.example.happyDream.Controller;
 
 import com.example.happyDream.DTO.UserDTO;
 import com.example.happyDream.Service.UserService;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,4 +21,8 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping("/users")
+    public String reviewSelectAll() {
+        return "userManagement";
+    }
 }
--- src/main/java/com/example/happyDream/Repository/ReviewRepository.java
+++ src/main/java/com/example/happyDream/Repository/ReviewRepository.java
@@ -1,8 +1,19 @@
 package com.example.happyDream.Repository;
 
+import com.example.happyDream.Entity.ChargerEntity;
 import com.example.happyDream.Entity.ReviewEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Integer>, ReviewRepositoryCustom {
+    @Query("SELECT r FROM ReviewEntity r JOIN r.chargerId c WHERE c IN :chargerIdList")
+    List<ReviewEntity> findByAddress(@Param("chargerIdList") List<ChargerEntity> chargerIdList);
 
+    @Query("SELECT r FROM ReviewEntity r WHERE r.reviewContent LIKE %:keyword%")
+    List<ReviewEntity> findBySubject(@Param("keyword") String keyword);
+
+    @Query("SELECT r FROM ReviewEntity r JOIN r.chargerId c WHERE c IN :chargerIdList AND r.reviewContent LIKE %:keyword%")
+    List<ReviewEntity> findByAddressAndSubject(@Param("chargerIdList") List<ChargerEntity> chargerIdList, @Param("keyword") String keyword);
 }
