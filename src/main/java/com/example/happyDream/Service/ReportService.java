95741b745854445f8b110eed77ad0baf11ffc48b
Seongmo Ahn <djgood780@gmail.com>
2024-11-21 18:17:26 +0900

    feat(Report): 고장 신고 처리 기능 추가

---
+++ src/main/java/com/example/happyDream/Service/ReportService.java
@@ -0,0 +1,87 @@
+package com.example.happyDream.Service;
+
+import com.example.happyDream.DTO.ReportDTO;
+import com.example.happyDream.DTO.ReviewDTO;
+import com.example.happyDream.Entity.ChargerEntity;
+import com.example.happyDream.Entity.ReportEntity;
+import com.example.happyDream.Entity.ReviewEntity;
+import com.example.happyDream.Repository.ReportRepository;
+import com.example.happyDream.Repository.ReportSpecification;
+import com.example.happyDream.Repository.ReviewRepository;
+import com.example.happyDream.Util.Converter;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReportService {
+    private final ChargerService chargerService;
+    private final ReportRepository reportRepository;
+
+    @Autowired
+    public ReportService(ChargerService chargerService, ReportRepository reportRepository) {
+        this.chargerService = chargerService;
+        this.reportRepository = reportRepository;
+    }
+
+    public List<ReportDTO> reportSelectAll() {
+        List<ReportEntity> entityList = this.reportRepository.findAll();
+        return Converter.EntityListToDtoList(entityList, ReportEntity::toDTO);
+    }
+
+    public void reportCheck(Integer reportId) {
+        ReportEntity report = this.reportRepository.findById(reportId)
+                .orElseThrow(() -> new IllegalArgumentException("신고를 찾을 수 없습니다."));
+        report.updateCheckedReport(true);
+        this.reportRepository.save(report);
+    }
+
+    public void reportRepair(Integer reportId) {
+        ReportEntity report = this.reportRepository.findById(reportId)
+                .orElseThrow(() -> new IllegalArgumentException("신고를 찾을 수 없습니다."));
+        report.updateReportState(true);
+        this.reportRepository.save(report);
+    }
+
+    public Map<String, List<String>> reportAddress() {
+        return createUniqueCities(this.chargerService.selectChargerAddress());
+    }
+
+    public List<ReportDTO> reportSelectAsSearch(String city, String district, Integer chargerId, Integer reportReason,
+                                                LocalDateTime startDate, LocalDateTime endDate) {
+        List<ChargerEntity> chargerIdList = this.chargerService.selectChargerCityAnddistrict(city, district);
+        Specification<ReportEntity> spec = ReportSpecification.combineConditions(chargerIdList, chargerId, reportReason, startDate, endDate);
+        return Converter.EntityListToDtoList(reportRepository.findAll(spec), ReportEntity::toDTO);
+    }
+
+    private Map<String, List<String>> createUniqueCities (List<Object[]> cities){
+        Map<String, List<String>> uniqueCities = cities.stream()
+                .filter(city -> city.length > 1 && city[0] instanceof String && city[1] instanceof String)
+                .collect(Collectors.toMap(
+                        city -> normalizeProvinceName((String) city[0]),
+                        city -> new ArrayList<>(List.of((String) city[1])), // 값을 List로 저장
+                        (existing, replacement) -> { // 중복된 key가 있을 때, 기존 리스트에 추가
+                            existing.addAll(replacement);
+                            return existing;
+                        }
+                ));
+        return uniqueCities;
+    }
+
+    // 도시 이름을 정규화하는 메서드
+    private String normalizeProvinceName(String provinceName) {
+        // 전북특별자치도 -> 전라북도, 강원특별자치도 -> 강원도
+        if ("전북특별자치도".equals(provinceName)) {
+            return "전라북도";
+        }
+        if ("강원특별자치도".equals(provinceName)) {
+            return "강원도";
+        }
+        return provinceName; // 그대로 반환
+    }
+}
